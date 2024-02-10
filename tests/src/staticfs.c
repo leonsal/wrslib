68182b20178554122801b4427f79f9cbba1df696
leonsal <leonel.salvadori@gmail.com>
2024-02-10 15:20:52 -0300

    ...

--- tests/src/staticfs.c
+++ tests/src/staticfs.c
@@ -1,8 +1,9 @@
 //
-// Embeds static filesystem in the executable
+// Embeds static filesystem in this translation unit
+// The directory of the zip file is relative to CMake build directory
 //
 #include "incbin.h"
-INCBIN(StaticfsZip, "../staticfs.zip");
+INCBIN(StaticfsZip, "../src/staticfs.zip");
 extern const unsigned char gStaticfsZipData[];
 extern const unsigned int  gStaticfsZipSize;
 
